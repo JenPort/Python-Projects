895b98c09a7613a879d6f4494aa45de1df2ccf34
Jenesis <40256583+JenPort@users.noreply.github.com>
2026-04-05 12:20:44 -0400

    JenesisAssignment3.py

--- JenesisAssignment3.py
+++ JenesisAssignment3.py
@@ -1,4 +1,4 @@
-# A BMI Calculator that doesn't make you feel to bad
+# A BMI Calculator that doesn't make you feel too bad
 def calculate_bmi(weight, height):
     bmi = weight / (height ** 2)
     return bmi
